1515f8ccee310b05bb86ae050b71a7ff617fc5af
qjarl1119 <qjarl1119@naver.com>
2021-02-03 13:33:16 +0900

    Form

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
@@ -68,8 +69,8 @@ class App extends Component{
   {
     const {classes} =this.props;
     return(
+      <div>
       <Paper className={classes.root}>
-        
         <Table className={classes.table}>
         <TableHead>
           <TableCell>번호</TableCell>
@@ -78,28 +79,16 @@ class App extends Component{
           <TableCell>생년월일</TableCell>
           <TableCell>성별</TableCell>
           <TableCell>직업</TableCell>
-
-        </TableHead>
-        
-        
-        <TableBody>
+        </TableHead> <TableBody>
         {this.state.customers ? this.state.customers.map(customer=> {return (<Customer key = {customer.id} id = {customer.id} name = {customer.name} birthday={customer.birthday} gender={customer.gender} job ={customer.job} image ={customer.image}>  </Customer>)
         }): 
-        <TableRow>
-            <TableCell colSpan="6" align="center">
+        <TableRow><TableCell colSpan="6" align="center">
           <CircularProgress className={classes.progress} variant="determinate" value ={this.state.completed}>
-          </CircularProgress>
-            </TableCell>
-        </TableRow>
-        
-        }
-      
-        </TableBody>
-        </Table>
-        
-      
-
-      </Paper>
+          </CircularProgress> </TableCell> </TableRow>
+        } </TableBody> </Table> </Paper>
+      <CustomerAdd></CustomerAdd>
+      </div>
+     
     );
   }
 }
